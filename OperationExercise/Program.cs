f4f0fa74c10fe18637db24e1a9ef04a8a3b25f5b
Colby Soldan <colbylsoldan@gmail.com>
2022-06-01 08:27:21 -0500

    first commit

---
+++ OperationExercise/Program.cs
@@ -0,0 +1,103 @@
+﻿using System;
+
+namespace OperationExercise
+{
+    class Program { 
+
+    public static double AreaOfCircle(double r)
+    {
+            return Math.PI * Math.Pow(r, 2);
+        }
+
+
+        static void Main(string[] args)
+        {
+            //Exercise 1//
+
+            int a;
+            int b;
+            string one = "Choose a number: ";
+            string two = "Choose another number: ";
+
+            //addition block
+            try
+            {
+                Console.Write(one);
+                a = Convert.ToInt32(Console.ReadLine());
+                Console.Write(two);
+                b = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine($"{a}+{b} is {a + b}.");
+            }
+            catch (FormatException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+            //subtraction block
+            try
+            {
+                Console.Write("\n" + one);
+                a = Convert.ToInt32(Console.ReadLine());
+                Console.Write(two);
+                b = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine($"{a}-{b} is {a - b}.");
+            }
+            catch (FormatException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+            //multiplication block
+            try
+            {
+                Console.Write("\n" + one);
+                a = Convert.ToInt32(Console.ReadLine());
+                Console.Write(two);
+                b = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine($"{a}*{b} is {a * b}.");
+            }
+            catch (FormatException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+            //division with remainder block
+            try
+            {
+                Console.Write("\n" + one);
+                a = Convert.ToInt32(Console.ReadLine());
+                Console.Write(two);
+                b = Convert.ToInt32(Console.ReadLine());
+                int quotient = a / b;
+                int remainder = a % b;
+
+                Console.WriteLine($"{a}/{b} is {quotient} remainder {remainder}.");
+            }
+            catch (DivideByZeroException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            catch (FormatException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        
+            Console.ReadLine();
+
+            //----------------------------------------------------//
+
+            //Exercise 2
+        
+            Console.Write("What is the radius of your circle? ");
+            var radius = double.Parse(Console.ReadLine());
+            var area = AreaOfCircle(radius);
+
+            Console.WriteLine($"The area of your circle with a radius of {radius} is {area}");
+
+            Console.ReadLine();
+
+
+        }
+    }
+}
+
---
+++ README.md
@@ -0,0 +1,5 @@
+# OperatorExercise
+
+I wantedto see what would happen if I violated the rules of this program, so I entered in values that weren't integers and that would quit my program.
+I also tried to divide by zero with the same result.
+I used try/catch to get around this, but I'm wondering if there's a way to make it less repetitive. Also, is there a way to use this so that they can try the operation again instead of leaving them on an error and moving on to the next one?
